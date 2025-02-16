973ec7ae7faa87003ed5de0f404bee974730d5b1
fpezcara <fpezcara@gmail.com>
2025-02-16 00:01:27 +0000

    Rescue params missing, not found and invalid record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,25 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActionController::ParameterMissing,
+             with: :rescue_from_parameter_missing
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_not_found_status
+
+  rescue_from ActiveRecord::RecordInvalid, with: :rescue_from_invalid_record
+
+  private
+
+  def rescue_from_parameter_missing(exception)
+    render json: { message: exception.message }, status: :bad_request
+  end
+
+  def rescue_from_not_found_status(exception)
+    render json: { message: exception.message }, status: :not_found
+  end
+
+  def rescue_from_invalid_record(exception)
+    render json: { message: exception.message }, status: :bad_request
+  end
 end
