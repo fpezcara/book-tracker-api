608de597d6f3dd003fca8f2d529a7d557b565d27
fpezcara <fpezcara@gmail.com>
2025-02-16 18:10:59 +0000

    Update the way we send data back from GoogleAPIService

--- app/services/google_books_service.rb
+++ app/services/google_books_service.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require "faraday"
-# todo: add validation so it can only be called with "intitle", "inauthor" or "isbn"
+
 class GoogleBooksService
   BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
-  ALLOWED_SEARCH_BY = %w[intitle inauthor isbn]
+  ALLOWED_SEARCH_BY = %w[title author isbn]
 
   def self.fetch_books(query, search_by)
     unless ALLOWED_SEARCH_BY.include?(search_by)
@@ -14,9 +14,12 @@ class GoogleBooksService
     url = build_url(query, search_by)
 
     response = Faraday.get(url)
-
+    # todo: probably will need to revise how many books we send back
     if response.success?
-      { data: JSON.parse(response.body) }
+      data = JSON.parse(response.body)["items"]
+      formatted_books = data.map { |book| formatted_data(book) }
+
+      { data: formatted_books }
     else
       { error: "Request failed with status #{response.status}" }
     end
@@ -24,6 +27,28 @@ class GoogleBooksService
 
   private
     def self.build_url(query, search_by)
-      "#{BASE_URL}#{search_by}:#{query}&orderBy=relevance&key=#{Rails.application.credentials.google_books_api_key}"
+      "#{BASE_URL}#{map_search_by(search_by)}:#{query}&orderBy=relevance&key=#{Rails.application.credentials.google_books_api_key}"
+    end
+
+    def self.map_search_by(search_by)
+      case search_by
+      when "title"
+        "intitle"
+      when "author"
+        "inauthor"
+      when "isbn"
+        "isbn"
+      end
+    end
+
+    def self.formatted_data(book)
+      {
+        title: book["volumeInfo"]["title"],
+        authors: book["volumeInfo"]["authors"],
+        published_date: book["volumeInfo"]["publishedDate"],
+        page_count: book["volumeInfo"]["pageCount"],
+        cover_image: book["volumeInfo"]["imageLinks"]["thumbnail"],
+        isbn: book["volumeInfo"]["industryIdentifiers"]&.first&.dig("identifier")
+      }
     end
 end
--- test/services/google_books_service_test.rb
+++ test/services/google_books_service_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class GoogleBooksServiceTest < ActiveSupport::TestCase
   def setup
-    @query = "harry potter"
-    @search_by = "intitle"
+    @query = "first day"
+    @search_by = "title"
     @api_key = Rails.application.credentials.google_books_api_key
     @base_url = "https://www.googleapis.com/books/v1/volumes?q="
   end
@@ -14,22 +14,22 @@ class GoogleBooksServiceTest < ActiveSupport::TestCase
       "items" => [
         {
           "volumeInfo" => {
-            "title" => "Harry Potter and the Sorcerer's Stone",
-            "authors" => [ "J.K. Rowling" ],
-            "publishedDate" => "1997-06-26",
-            "pageCount" => 309,
+            "title" => "Jake's First Day",
+            "authors" => [ "Twinkl Originals" ],
+            "publishedDate" => "2019-05-29",
+            "pageCount" => 18,
             "imageLinks" => {
-              "thumbnail" => "https://example.com/thumbnail.jpg"
+              "thumbnail" => "http://books.google.com/books/content?id=CRgrDwAAQBAJ\u0026printsec=frontcover\u0026img=1\u0026zoom=1\u0026edge=curl\u0026source=gbs_api"
             },
             "industryIdentifiers" => [
-              { "identifier" => "9780747532699" }
+              { "identifier" => "9780241322741" }
             ]
           }
         }
       ]
     }.to_json
 
-    stub_request(:get, "#{@base_url}#{@search_by}:#{@query}&orderBy=relevance&key=#{@api_key}").
+    stub_request(:get, "#{@base_url}intitle:#{@query}&orderBy=relevance&key=#{@api_key}").
       with(
         headers: {
               "Accept"=>"*/*",
@@ -37,19 +37,36 @@ class GoogleBooksServiceTest < ActiveSupport::TestCase
               "User-Agent"=>"Faraday v2.12.2"
         }).to_return(status: 200, body: response_body, headers: {})
 
-
+    expected_response = [
+        {
+           title: "Jake's First Day",
+            authors:  [ "Twinkl Originals" ],
+            published_date: "2019-05-29",
+            page_count: 18,
+            cover_image: "http://books.google.com/books/content?id=CRgrDwAAQBAJ\u0026printsec=frontcover\u0026img=1\u0026zoom=1\u0026edge=curl\u0026source=gbs_api",
+            isbn: "9780241322741"
+        }
+      ]
 
     books = GoogleBooksService.fetch_books(@query, @search_by)
 
-    assert_equal({ data: JSON.parse(response_body) }, books)
+    assert_equal({ data: expected_response }, books)
   end
 
   def test_fetch_books_failure
-    stub_request(:get, "#{@base_url}#{@search_by}:#{@query}&orderBy=relevance&key=#{@api_key}")
+    stub_request(:get, "#{@base_url}intitle:#{@query}&orderBy=relevance&key=#{@api_key}")
       .to_return(status: 500, body: "", headers: {})
 
     books = GoogleBooksService.fetch_books(@query, @search_by)
 
     assert_equal({ error: "Request failed with status 500" }, books)
+  end
+
+  def test_fetch_books_with_wrong_search_by_option
+    search_by = "wrong_search_by"
+
+    books = GoogleBooksService.fetch_books(@query, search_by)
+
+    assert_equal({ error: "Invalid search_by parameter" }, books)
   end
 end
