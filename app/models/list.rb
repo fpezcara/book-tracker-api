e5499a70797442f6c5aa1ce2cb70a2f36a8345a7
fpezcara <fpezcara@gmail.com>
2025-03-02 21:06:31 +0000

    Fix validation test that were false positives

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,4 +6,9 @@ class List < ApplicationRecord
   #  todo: create default lists for users (reading, to-read, completed) => hacerlo en el users model
   # def self.create_default_lists(user) - is the user param needed?
   validates :name, presence: true, uniqueness: true
+  before_save :capitalize_name
+
+  def capitalize_name
+   self.name = name&.capitalize
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = FactoryBot.create(:user)
+  end
+
+  test "valid user" do
+    assert @user.valid?
+    assert_equal 1, User.count
+  end
+
+  test "invalid when email_address is missing" do
+    @user.email_address = nil
+    # trigger validation
+    @user.valid?
+
+    assert_not @user.valid?
+    assert_equal ["Email address can't be blank"], @user.errors.full_messages
+  end
+
+  test "invalid when password is missing" do
+    @user.password = nil
+    @user.valid?
+
+    assert_not @user.valid?
+    assert_equal ["Password can't be blank"], @user.errors.full_messages
+  end
 end
