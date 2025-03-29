f162430db67ce7da93c40239da799a4b8173e514
fpezcara <fpezcara@gmail.com>
2025-03-29 18:04:24 +0000

    Update cookie config to fix prod

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 Rails.application.config.session_store :cookie_store,
   key: "_book_tracker_session",
-  same_site: Rails.env.production? ? :none : :lax,  # Use :none in production, :lax in development
-  secure: Rails.env.production?,  # Set cookies as secure only in production
-  domain: Rails.env.production? ? "book-tracker-alpha.vercel.app" : :all,  # Use production domain in production and :all for development
+  same_site: Rails.env.production? ? :none : :lax,
+  secure: true,
+  domain: Rails.env.production? ? "book-tracker-backend-lpy4.onrender.com" : :all,
   http_only: true,
   expire_after: 1.day
