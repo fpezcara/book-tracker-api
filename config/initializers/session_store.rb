d1dd5883f1cbf5e2f164f69dd896733635ffe086
fpezcara <fpezcara@gmail.com>
2025-03-30 17:00:22 +0100

    Only make cookies secure in prod

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 Rails.application.config.session_store :cookie_store,
   key: "_book_tracker_session",
   same_site: Rails.env.production? ? :none : :lax,
-  secure: true,
+  secure: Rails.env.production?,
   domain: Rails.env.production? ? "book-tracker-backend-lpy4.onrender.com" : :all,
   http_only: true,
   expire_after: 1.day
