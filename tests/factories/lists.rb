3cba12743810326cd3b92ed7a577be0f65ac688f
fpezcara <fpezcara@gmail.com>
2025-04-19 02:41:26 +0100

    Remove tests folder

--- tests/factories/lists.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :list do
-    name { "Fiction" }
-    association :user
-  end
-end
--- tests/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    sequence(:email_address) { |n| "user#{n}@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
-  end
-end
